ffcfd9ac92c2d09c06a6570b5d4803e00a4e8bd0
Pavel Grebnev <ruwhynot@gmail.com>
2023-05-20 16:41:33 +0200

    Fix shortcut bat script

--- data/windows/spr.bat
+++ data/windows/spr.bat
@@ -1,7 +1,10 @@
 @echo off
 
 set SCRIPT_DIR=%~dp0
-set SCRIPTER_DIR=%SCRIPT_DIR\.
-set LOG_FILE=scripter_logs\last_log_%RANDOM%.txt
+set SCRIPTER_DIR=%SCRIPT_DIR%
+set LOG_FOLDER=%SCRIPTER_DIR%\scripter_logs
+set LOG_FILE=%LOG_FOLDER%\last_log_%RANDOM%.txt
+
+if not exist "%LOG_FOLDER%" mkdir %LOG_FOLDER%
 
 start %SCRIPTER_DIR%\scripter.exe > %LOG_FILE% 2>&1
